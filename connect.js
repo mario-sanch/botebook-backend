374cf5da8286188df1d2edf40a6e1a4d786fbd51
Mario <mailbox.mariosc@gmail.com>
2025-10-19 23:43:31 +0100

    first commit, connect to database, and set up first routes

---
+++ connect.js
@@ -0,0 +1,36 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config({ path: "./config.env" });
+
+const client = new MongoClient(process.env.ATLAS_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+let database;
+
+module.exports = {
+  connectToServer: () => {
+    database = client.db("blogData");
+  },
+  getDb: () => {
+    return database;
+  },
+};
+
+/*
+async function run() {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);*/
---
+++ server.js
@@ -0,0 +1,17 @@
+const connect = require("./connect");
+const express = require("express");
+const cors = require("cors");
+const posts = require("./postRoutes");
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(posts);
+
+app.listen(PORT, () => {
+  console.log(`Server connected to port ${PORT}`);
+  connect.connectToServer();
+  console.log(`Connected to server`);
+});
